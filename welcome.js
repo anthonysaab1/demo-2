21fc285d9ba817d861b7dc776af938246ef3645d
Anthony <nourhan.farhat@eulerts.com>
2024-05-17 13:03:54 +0300

    filtration

--- welcome.js
+++ welcome.js
@@ -9,26 +9,18 @@ let saveBtn = document.getElementById("saveBtn");
 let addBtn = document.getElementById("addBtn");
 let errorMsg = document.getElementById("errorMsg");
 const loader = document.getElementById("loader");
-let allData;
+let allData = [];
 let userId;
 let allEditedData = [];
-const start = performance.now();
-let newData = [];
+
 let addedTable = localStorage.addItem ? JSON.parse(localStorage.addItem) : [];
-let LScompleted = localStorage.completed
-  ? JSON.parse(localStorage.completed)
-  : [];
 const deletedItem = localStorage.deletedItem
   ? JSON.parse(localStorage.deletedItem)
   : [];
-
-const taskNameLS = localStorage.taskName ? localStorage.taskName : "";
-const taskIdLS = localStorage.taskId ? JSON.parse(localStorage.taskId) : "";
 const editDataLS = localStorage.editData
   ? JSON.parse(localStorage.editData)
   : [];
 
-let editData = [];
 let editDataObj = {};
 outputMsg.textContent = `Welcome ${localStorage.lastname}`;
 logoutBtn.addEventListener("click", () => {
@@ -43,7 +35,7 @@ fetch("https://jsonplaceholder.typicode.com/todos")
     allEditedData = data;
     let addDataLs = localStorage.addItem
       ? JSON.parse(localStorage.addItem)
-      : [{}];
+      : [];
     for (let i = 0; i < addDataLs.length; i++) {
       data.push(addDataLs[i]);
     }
@@ -51,174 +43,126 @@ fetch("https://jsonplaceholder.typicode.com/todos")
     data.forEach((todo) => {
       createTable(todo);
     });
-    const end = performance.now();
-    // const elapsedTime = end - start;
+
     loader.style.display = "none";
-    // console.log(`Time taken to fetch data: ${elapsedTime} milliseconds`);
   });
 
 function createTable(todo) {
-  let dataArray = deletedItem;
+  let editBtn = document.createElement("button");
+  let editRow = document.createElement("td");
+  editBtn.innerHTML = editMark;
+  let trashBtn = document.createElement("button");
+  let trashRow = document.createElement("td");
+  let tableRow = document.createElement("tr");
+  tableRow.setAttribute("id", todo.id);
+  let taskNameRow = document.createElement("td");
+
+  let completed = document.createElement("td");
   loader.style.display = "block";
-
-  if (!dataArray.includes(todo.id)) {
-    if (todo.userId == JSON.parse(localStorage.id)) {
-      userId = todo.userId;
-      let tableRow = document.createElement("tr");
-      tableRow.setAttribute("id", todo.id);
-      let taskNameRow = document.createElement("td");
-
-      let completed = document.createElement("td");
-      let LS = localStorage.editData ? JSON.parse(localStorage.editData) : [{}];
-      for (let i = 0; i < LS.length; i++) {
-        if (LS[i].id == todo.id) {
-          taskNameRow.textContent = LS[i].title;
-          if (LS[i].completed == true) {
-            completed.innerHTML = completedMark;
-            break;
-          } else {
-            completed.innerHTML = unCompletedMark;
-            break;
-          }
-        } else {
-          taskNameRow.textContent = todo.title;
-        }
-      }
-
-      let editBtn = document.createElement("button");
-      let editRow = document.createElement("td");
-      editBtn.innerHTML = editMark;
-      let trashBtn = document.createElement("button");
-      let trashRow = document.createElement("td");
-      trashBtn.innerHTML = trashMark;
-      for (let i = 0; i < editDataLS.length; i++) {
-        if (editDataLS[i].id == todo.id) {
-          todo.title = editDataLS[i].title;
-          todo.completed = editDataLS[i].completed;
-          break;
-        }
-      }
-      if (todo.completed == true || todo.completed == "true") {
-        completed.innerHTML = completedMark;
-        todo.completed = true;
-      } else {
-        completed.innerHTML = unCompletedMark;
-      }
-      trashRow.append(trashBtn);
-      editRow.append(editBtn);
-      tableRow.append(taskNameRow, completed, editRow, trashRow);
-
-      MainTable.append(tableRow);
-      editfunc(editBtn, todo);
-      deleteFunc(trashBtn, todo);
+  if (
+    deletedItem.includes(todo.id) ||
+    todo.userId != JSON.parse(localStorage.id)
+  ) {
+    return;
+  }
+  userId = todo.userId;
+  taskNameRow.textContent = todo.title;
+  for (let i = 0; i < editDataLS.length; i++) {
+    if (editDataLS[i].id == todo.id) {
+      taskNameRow.textContent = editDataLS[i].title;
+      completed.innerHTML = editDataLS[i].completed
+        ? completedMark
+        : unCompletedMark;
+      todo.completed = editDataLS[i].completed;
+      todo.title = editDataLS[i].title;
     }
   }
+
+  trashBtn.innerHTML = trashMark;
+  completed.innerHTML = todo.completed ? completedMark : unCompletedMark;
+  trashRow.append(trashBtn);
+  editRow.append(editBtn);
+  tableRow.append(taskNameRow, completed, editRow, trashRow);
+
+  MainTable.append(tableRow);
+  editfunc(editBtn, todo);
+  deleteFunc(trashBtn, todo);
 }
 
 function editfunc(editBtn, todo) {
   editBtn.addEventListener("click", async function editObjectById() {
-    taskName.value = todo.title;
     userIdCheck.value = todo.id;
+    let todoRow = document.getElementById(userIdCheck.value);
+    taskName.value = todoRow.children[0].textContent;
     addBtn.style.display = "none";
     saveBtn.style.display = "block";
-    for (let i = 0; i < editDataLS.length; i++) {
-      if (todo.id == editDataLS[i].id) {
-        userIdCheck.value = editDataLS[i].id;
-        taskName.readOnly = false;
-        taskName.value = editDataLS[i].title;
-        break;
-      }
-    }
-
-    if (todo.completed == true || todo.completed == "true") {
-      isCompleted.checked = true;
-      isCompleted.disabled = true;
-    } else {
-      isCompleted.checked = false;
-      isCompleted.disabled = false;
-    }
+    isCompleted.checked = isCompleted.disabled = todo.completed;
   });
 }
 //Save btn
 saveBtn.addEventListener("click", async function addObject() {
   loader.style.display = "block";
-  if (taskName.value.length == 0) {
-    errorMsg.textContent = "Hit the pen button";
-  } else {
-    let todoRow = document.getElementById(userIdCheck.value);
+  let todoRow = document.getElementById(userIdCheck.value);
+  let completed;
 
-    allEditedData.forEach((todo) => {
-      if (todo.title == taskName.value) {
-        isCompleted.checked = false;
-        isCompleted.disabled = false;
-        loader.style.display = "none";
-        saveBtn.style.display = "none";
-        taskName.value = "";
-        addBtn.style.display = "block";
-      } else {
-        let completed;
-        if (isCompleted.checked == true) {
-          todoRow.children[1].innerHTML = completedMark;
-          completed = true;
-          todo.completed = true;
-        } else {
-          completed = false;
+  allData.forEach((todo) => {
+    if (todo.title == taskName.value) {
+      falsy();
+      taskName.value = "";
+      addBtn.style.display = "block";
+      return;
+    }
+
+    if (isCompleted.checked) {
+      todoRow.children[1].innerHTML = completedMark;
+      todo.completed = true;
+      completed = true;
+    } else {
+      completed = false;
+    }
+
+    if (todo.title == taskName.value) {
+      taskName.value = "";
+      addBtn.style.display = "";
+      return;
+    }
+    if (todo.id == userIdCheck.value) {
+      localStorage.setItem("taskId", userIdCheck.value);
+      fetch(
+        `https://jsonplaceholder.typicode.com/posts/${userIdCheck.value}`,
+
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            userId: todo.userId,
+            id: todo.id,
+            title: taskName.value,
+            completed: completed,
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
         }
-        if (todo.title == taskName.value) {
-          taskName.value = "";
-          addBtn.style.display = "";
-        } else {
-          if (todo.id == userIdCheck.value) {
-            localStorage.setItem("taskId", userIdCheck.value);
-            fetch(
-              `https://jsonplaceholder.typicode.com/posts/${userIdCheck.value}`,
+      )
+        .then((response) => response.json())
+        .then((json) => console.log(json));
 
-              {
-                method: "PUT",
-                body: JSON.stringify({
-                  userId: `${todo.userId}`,
-                  id: `${todo.id}`,
-                  title: taskName.value,
-                  completed: `${completed}`,
-                }),
-                headers: {
-                  "Content-type": "application/json; charset=UTF-8",
-                },
-              }
-            )
-              .then((response) => response.json())
-              .then((json) => console.log(json));
+      editDataObj = {
+        userId: todo.userId,
+        id: todo.id,
+        title: taskName.value,
+        completed,
+      };
 
-            editDataObj = {
-              userId: `${todo.userId}`,
-              id: `${todo.id}`,
-              title: taskName.value,
-              completed: `${completed}`,
-            };
+      editDataLS.push(editDataObj);
+      localStorage.setItem("editData", JSON.stringify(editDataLS));
+      todoRow.children[0].textContent = taskName.value;
+      taskName.value = "";
+      addBtn.style.display = "";
+    }
+  });
 
-            editData.push(editDataObj);
-            for (let i = 0; i < editDataLS.length; i++) {
-              editData.push(editDataLS[i]);
-              if (editDataLS[i].id == todo.id) {
-                todo.title = editDataLS[i].title;
-                break;
-              }
-            }
-
-            localStorage.setItem("editData", JSON.stringify(editData));
-            todoRow.children[0].textContent = taskName.value;
-            taskName.value = "";
-
-            addBtn.style.display = "";
-          }
-        }
-      }
-    });
-  }
-  isCompleted.checked = false;
-  isCompleted.disabled = false;
-  loader.style.display = "none";
-  saveBtn.style.display = "none";
+  falsy();
 });
 //
 ////////
@@ -246,9 +190,8 @@ function deleteFunc(trashBtn, todo) {
         allData = allData.filter((element) => {
           element.id !== todo.id;
         });
-        const end = performance.now();
-        const elapsedTime = end - start;
-        console.log(`Time taken to fetch data: ${elapsedTime} milliseconds`);
+        console.log(allData);
+
         deletedItem.push(todo.id);
         localStorage.setItem("deletedItem", JSON.stringify(deletedItem));
         MainTable.removeChild(todoRow);
@@ -256,57 +199,50 @@ function deleteFunc(trashBtn, todo) {
     } catch (error) {
       console.error("Error:", error.message);
     }
-    loader.style.display = "none";
-    isCompleted.checked = false;
-    isCompleted.disabled = false;
+    falsy();
   });
 }
 //
 //Add to table
 let LSidAdded = localStorage.idAdded ? JSON.parse(localStorage.idAdded) : 200;
-addBtn.addEventListener("click", async function addObjectTotable() {
-  loader.style.display = "block";
-  if (taskName.value.length !== 0) {
-    LSidAdded++;
-    let completed;
-
-    if (isCompleted.checked == true) {
-      completed = true;
-    } else {
-      completed = false;
-    }
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify({
-        userId: `11`,
-        id: LSidAdded,
-        title: taskName.value,
-        completed: `${completed}`,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-    addedObj = {
-      userId: `${userId}`,
-      id: `${LSidAdded}`,
-      title: taskName.value,
-      completed: `${completed}`,
-    };
-
-    addedTable.push(addedObj);
-    localStorage.setItem("addItem", JSON.stringify(addedTable));
-    errorMsg.textContent = "";
-    taskName.value = "";
-    isCompleted.checked = false;
-    loader.style.display = "none";
-  } else {
+addBtn.addEventListener("click", function addObjectTotable() {
+  if (taskName.value.length == 0) {
     errorMsg.textContent = "Empty Title Name";
-    loader.style.display = "none";
+    return;
   }
+  loader.style.display = "block";
+  LSidAdded++;
+  let completed;
+  localStorage.setItem("idAdded", JSON.stringify(LSidAdded));
+
+  completed = isCompleted.checked;
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      userId: `11`,
+      id: LSidAdded,
+      title: taskName.value,
+      completed,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  addedObj = addedTable.push(addedObj);
+  localStorage.setItem("addItem", JSON.stringify(addedTable));
+  errorMsg.textContent = "";
+  taskName.value = "";
+  falsy();
 });
+////////
+function falsy() {
+  isCompleted.checked = false;
+  isCompleted.disabled = false;
+  loader.style.display = "none";
+  saveBtn.style.display = "none";
+}
 
 let unCompletedMark = `<svg
 xmlns="http://www.w3.org/2000/svg"
@@ -348,3 +284,4 @@ id="deleteBtn"
   d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"
 />
 </svg>`;
+console.log("sss");
