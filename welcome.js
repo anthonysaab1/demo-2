79fc8676ea72267525fc5a12a300614c92789269
Anthony <nourhan.farhat@eulerts.com>
2024-05-17 14:34:47 +0300

    neww

--- welcome.js
+++ welcome.js
@@ -20,6 +20,7 @@ const deletedItem = localStorage.deletedItem
 const editDataLS = localStorage.editData
   ? JSON.parse(localStorage.editData)
   : [];
+
 let editDataObj = {};
 outputMsg.textContent = `Welcome ${localStorage.lastname}`;
 logoutBtn.addEventListener("click", () => {
@@ -45,7 +46,7 @@ fetch("https://jsonplaceholder.typicode.com/todos")
 
     loader.style.display = "none";
   });
-// Create Table
+
 function createTable(todo) {
   let editBtn = document.createElement("button");
   let editRow = document.createElement("td");
@@ -64,7 +65,9 @@ function createTable(todo) {
   ) {
     return;
   }
+
   userId = todo.userId;
+
   taskNameRow.textContent = todo.title;
   for (let i = 0; i < editDataLS.length; i++) {
     if (editDataLS[i].id == todo.id) {
@@ -104,6 +107,10 @@ saveBtn.addEventListener("click", async function addObject() {
   let todoRow = document.getElementById(userIdCheck.value);
   let completed;
 
+  if (taskName.value.length == 0) {
+    errorMsg.textContent = "Hit the pen button";
+    return;
+  }
   allData.forEach((todo) => {
     if (todo.title == taskName.value) {
       falsy();
@@ -248,14 +255,7 @@ function falsy() {
   loader.style.display = "none";
   saveBtn.style.display = "none";
 }
-function repalce(userId, id, completed) {
-  addedObj = {
-    userId: userId,
-    id: id,
-    title: taskName.value,
-    completed,
-  };
-}
+
 let unCompletedMark = `<svg
 xmlns="http://www.w3.org/2000/svg"
 viewBox="0 0 384 512"
@@ -296,3 +296,4 @@ id="deleteBtn"
   d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"
 />
 </svg>`;
+console.log("sss");
